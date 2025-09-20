a5912cdc87a81b88ccdd674306fd0635edc66005
Sam Bonnekamp <sam@bonnekamp.net>
2025-09-20 21:52:50 +0800

    new compare

--- compare.c
+++ compare.c
@@ -3,7 +3,9 @@
 #include "sas_alloc.h"
 #include <time.h>
 
-#define AMOUNT_OF_ALLOCS 990000
+#define AMOUNT_OF_ALLOCS 500
+
+void *ptrs[AMOUNT_OF_ALLOCS];
 
 typedef struct{
   int x;
@@ -19,8 +21,10 @@ int main(int argc, char* argv[]){
   printf("malloc'ing %d addresses of size %zu\n", AMOUNT_OF_ALLOCS, ts);
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start1);
   for (long i = 0; i < AMOUNT_OF_ALLOCS; i++){
-    void* s = malloc(ts);
-    free(s);
+    ptrs[i] = malloc(ts);
+  }
+  for (long i = 0; i < AMOUNT_OF_ALLOCS; i++){
+    free(ptrs[i]);
   }  
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop1);
   double result = (stop1.tv_sec - start1.tv_sec) * 1e6 + (stop1.tv_nsec - start1.tv_nsec) / 1e3;    // in microseconds
@@ -30,10 +34,12 @@ int main(int argc, char* argv[]){
   printf("sas_alloc'ing %d addresses of size %zu\n", AMOUNT_OF_ALLOCS, ts);  
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start2);
   for (long i = 0; i < AMOUNT_OF_ALLOCS; i++){
-    void* s = sas_alloc();
-    sas_free(s);
+    ptrs[i] = sas_alloc();
   }
-  
+
+  for (long i = 0; i < AMOUNT_OF_ALLOCS; i++){
+    sas_free(ptrs[i]);
+  }
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop2);
   double result1 = (stop2.tv_sec - start2.tv_sec) * 1e6 + (stop2.tv_nsec - start2.tv_nsec) / 1e3;    // in microseconds
   printf("sas_alloc took %f ms\n", result1);
